3ce4a6ad217ae1e7da3722997d12a1ee949a3c36
Victor Cornejo <rex2002xp@gmail.com>
2022-09-27 22:11:07 -0600

    Version 19.0.2

--- generate-image-base.sh
+++ generate-image-base.sh
@@ -1,8 +1,8 @@
 #/bin/zsh
 
-VERSION=18.0.2 # set version here
+VERSION=19.0.2 # set version here
 
-cd /tmp/keycloak/v18.0.2
+cd /tmp/keycloak/v19.0.2
 git clone git@github.com:keycloak/keycloak-containers.git
 cd keycloak-containers/server
 git checkout $VERSION
