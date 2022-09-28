9f1e3f38203b15c1f1386ded99ea5d3b1ea07d68
Victor Cornejo <rex2002xp@gmail.com>
2022-09-27 22:22:54 -0600

    Actualizacion de Configuraciones

--- generate-image-base.sh
+++ generate-image-base.sh
@@ -2,6 +2,7 @@
 
 VERSION=19.0.2 # set version here
 
+mkdir -p /tmp/keycloak/v19.0.2
 cd /tmp/keycloak/v19.0.2
 git clone git@github.com:keycloak/keycloak-containers.git
 cd keycloak-containers/server
